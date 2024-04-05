1e505c62926a872a3968259ea8dc8696108af96a
nandinipoo <166119837+nandinipoo@users.noreply.github.com>
2024-04-05 15:40:24 +0530

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,15 @@
+package Labrecord;
+
+public class Lab1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello bro");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
